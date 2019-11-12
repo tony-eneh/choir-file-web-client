c5e68b77919b86e8fec0c8e2fe857a77b5d09eab
Tony Eneh <aeneh90@gmail.com>
2019-11-12 15:53:04 +0100

    Link to selection-details page from selections-list page

--- src/app/selections-list/selections-list.component.ts
+++ src/app/selections-list/selections-list.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import { SelectionsService } from '../selections.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-selections-list',
@@ -7,12 +8,13 @@ import { SelectionsService } from '../selections.service';
     styleUrls: ['./selections-list.component.css']
 })
 export class SelectionsListComponent {
-    constructor(private selectionsService: SelectionsService){}
+    constructor(private selectionsService: SelectionsService, private router: Router){}
 
     selections = this.selectionsService.getSelections();
 
     viewSelection(id){
         this.selectionsService.currentSelection = this.selectionsService.getSelection(id);
         console.log`${this.selectionsService.currentSelection}`;
+        this.router.navigate(['selections', id]);
     }
 }
