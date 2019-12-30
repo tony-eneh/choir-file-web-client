ed00c8332c650f3596dcc2118a90c82a3fcc0fb7
Tony Eneh <aeneh90@gmail.com>
2019-12-30 05:04:20 +0100

    Create docker component for dashboard and co

---
+++ src/app/docker/docker.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-docker',
+  templateUrl: './docker.component.html',
+  styleUrls: ['./docker.component.css']
+})
+export class DockerComponent implements OnInit {
+private userId: String;
+private userIcon;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
