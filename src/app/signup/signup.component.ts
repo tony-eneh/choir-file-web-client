330b1f15494ddea8bb9ff6edb988ea0db01de6fb
Tony Eneh <aeneh90@gmail.com>
2019-12-30 04:33:21 +0100

    implement signup page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import { Router } from '@angular/router';
+import { SignupService } from '../signup.service';
 
 @Component({
     selector: 'app-signup',
@@ -6,5 +8,54 @@ import {Component} from '@angular/core';
     styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-    onSubmit = (e) => {console.log`e`; e.preventDefault()};
+  private firstname='';
+  private lastname='';
+  private sex='';
+  private email='';
+  private phoneNo= '';
+  private username='';
+  private password='';
+  private messageArea;
+
+  constructor(private signupService: SignupService, private router: Router){}
+
+  ngOnInit = ()=>{
+    this.messageArea = document.querySelector('.alert');
+  }
+
+  onSubmit(e){
+
+    const newUser = {firstname: this.firstname,
+      lastname: this.lastname,
+      sex: this.sex,
+      email: this.email,
+      phoneNo: this.phoneNo,
+      username: this.username,
+      password: this.password
+    }
+
+    //check if username already exists
+    //display error if yes
+    //register user if no
+    this.signupService.signup(newUser).subscribe((registeredUserId)=>{
+        //if successfully registered redirect to dashboard
+        if(registeredUserId){
+            this.router.navigate(['/dashboard'])
+            //if router couldn't navigate display navigation error
+            .catch(this.displayNavigationError);
+        }else{
+            //else display registration error
+            this.displayRegistrationError();
+        }
+
+    })
+  };
+
+  displayRegistrationError(){
+    this.messageArea.textContent = 'Username of password is incorrect';
+  }
+
+  displayNavigationError(){
+    this.messageArea.textContent= 'Sorry we temporarily can\'t register you. Try again shortly';
+  }
 }
